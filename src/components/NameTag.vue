1c247ff54a97163e04ead5511194f699cefa5bde
Ryan Durham <ryan@stagerightlabs.com>
2018-05-03 14:27:11 -0700

    Add cancel button to name tag component

--- src/components/NameTag.vue
+++ src/components/NameTag.vue
@@ -11,6 +11,9 @@
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="name" type="text" placeholder="New Name" v-model="name">
           </div>
           <div class="flex justify-end">
+            <button class="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 mr-4 rounded" type="button" @click="close">
+              Cancel
+            </button>
             <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button" @click="submit">
               Save
             </button>
