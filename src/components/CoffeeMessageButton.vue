7fa23ef4a1ab348fbfff4f0afd6704744f882880
Ryan Durham <ryan@stagerightlabs.com>
2018-05-03 10:35:13 -0700

    Extract coffe-message-button component

---
+++ src/components/CoffeeMessageButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button @click="show" class="bg-teal hover:bg-teal-dark text-white font-bold py-2 px-4 m-4 rounded cursor-pointer">
+    <slot></slot>
+    <portal to="modals" v-if="visible">
+      <modal @closed="close">
+        <div class="text-black font-bold text-xl mb-2">Can coffee make you a better developer?</div>
+        <p class="text-grey-darker text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+      </modal>
+    </portal>
+  </button>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    show() {
+      this.visible = true
+    },
+    toggle() {
+      this.visible = ! this.visible
+    },
+    close() {
+      this.visible = false
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,24 @@
   <div class="home text-center">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illum autem itaque nostrum dolorum, consequatur neque quidem impedit quas ab tempore magnam praesentium voluptatem dolores! Dolore iste rerum sunt id.</p>
     <p>
-      <a @click="showModal = true" class="bg-teal hover:bg-teal-dark text-white font-bold py-2 px-4 m-4 rounded cursor-pointer">Show Modal</a>
+      <coffee-message-button>Hello Coffee!</coffee-message-button>
     </p>
-    <modal v-if="showModal" @closed="showModal = false">
-      <div class="text-black font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-      <p class="text-grey-darker text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-    </modal>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import Modal from '@/components/Modal.vue'
+import CoffeeMessageButton from '@/components/CoffeeMessageButton.vue'
 
 export default {
   name: 'home',
   components: {
-    Modal
+    CoffeeMessageButton
   },
   data() {
     return {
-      showModal: false
+
     }
   }
 };
