7d324ec6a62ebd553aa08c57afeebf75e312d3da
Ryan Durham <ryan@stagerightlabs.com>
2018-05-03 14:00:48 -0700

    Set visible modal to be 100% of the width alloted by the mask div

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="absolute pin w-full h-full bg-smoke-light flex" @click.self="close">
     <div class="max-w-md w-full lg:flex self-center m-auto">
-      <div class="border border-grey-light lg:border lg:border-grey-light bg-white  lg:rounded p-4 flex flex-col justify-between leading-normal">
+      <div class="border border-grey-light lg:border lg:border-grey-light bg-white lg:rounded p-4 flex flex-col justify-between leading-normal w-full">
         <div class="mb-8">
           <span @click="close">
             <icon name="times" class="float-right cursor-pointer"></icon>
