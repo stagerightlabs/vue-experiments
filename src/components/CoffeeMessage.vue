44b3789d6d415fd5c584711c39889182b4bab8fb
Ryan Durham <ryan@stagerightlabs.com>
2018-05-03 13:46:57 -0700

    Refactor CoffeeMessage Modal

--- src/components/CoffeeMessage.vue
+++ src/components/CoffeeMessage.vue
@@ -1,8 +1,8 @@
 <template>
   <button @click="show" class="bg-teal hover:bg-teal-dark text-white font-bold py-2 px-4 m-4 rounded cursor-pointer">
-    <slot></slot>
+    <slot>Click Me!</slot>
     <portal to="modals" v-if="visible">
-      <modal @closed="close">
+      <modal @modal-closed="close" :modal-visible="visible">
         <div class="text-black font-bold text-xl mb-2">Can coffee make you a better developer?</div>
         <p class="text-grey-darker text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
       </modal>
@@ -25,9 +25,6 @@ export default {
   methods: {
     show() {
       this.visible = true
-    },
-    toggle() {
-      this.visible = ! this.visible
     },
     close() {
       this.visible = false
