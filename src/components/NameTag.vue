1f81c0b431ac7cdbb4946caff21bff94de91fff2
Ryan Durham <ryan@stagerightlabs.com>
2018-05-03 14:06:01 -0700

    Create 'name tag' modal component

---
+++ src/components/NameTag.vue
@@ -0,0 +1,51 @@
+<template>
+  <button @click="show" class="bg-teal hover:bg-teal-dark text-white font-bold py-2 px-4 m-4 rounded cursor-pointer">
+    <slot>What is your name?</slot>
+    <portal to="modals" v-if="visible">
+      <modal @modal-closed="close" :modal-visible="visible">
+        <form @submit.prevent="submit">
+          <div class="mb-4">
+            <label class="block text-grey-darker text-sm font-bold mb-2 text-xl" for="name">
+              What is your name?
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="name" type="text" placeholder="New Name" v-model="name">
+          </div>
+          <div class="flex justify-end">
+            <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button" @click="submit">
+              Save
+            </button>
+          </div>
+        </form>
+      </modal>
+    </portal>
+  </button>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      visible: false,
+      name: '',
+    }
+  },
+  methods: {
+    show() {
+      this.visible = true
+    },
+    close() {
+      this.visible = false
+    },
+    submit() {
+      this.$emit('new-name', this.name)
+      this.name = ''
+      this.close()
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home text-center">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illum autem itaque nostrum dolorum, consequatur neque quidem impedit quas ab tempore magnam praesentium voluptatem dolores! Dolore iste rerum sunt id.</p>
     <p>
-      <coffee-message-button>Hello Coffee!</coffee-message-button>
+      <coffee-button>Hello {{ name }}!</coffee-button>
+      <name-tag-button @new-name="changeName">My Name is...</name-tag-button>
     </p>
   </div>
 </template>
@@ -10,16 +11,23 @@
 <script>
 // @ is an alias to /src
 
-import CoffeeMessageButton from '@/components/CoffeeMessageButton.vue'
+import CoffeeMessage from '@/components/CoffeeMessage.vue'
+import NameTag from '@/components/NameTag.vue'
 
 export default {
   name: 'home',
   components: {
-    CoffeeMessageButton
+    'coffee-button': CoffeeMessage,
+    'name-tag-button': NameTag,
   },
   data() {
     return {
-
+      name: 'Coffee'
+    }
+  },
+  methods: {
+    changeName(name) {
+      this.name = name
     }
   }
 };
