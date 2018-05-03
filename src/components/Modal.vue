4929e4c9af73bf35f29f4ff8836734f715730b19
Ryan Durham <ryan@stagerightlabs.com>
2018-05-03 13:47:12 -0700

    Change base modal event names to be more clear

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,4 +1,4 @@
-  <template>
+<template>
   <div class="absolute pin w-full h-full bg-smoke-light flex" @click.self="close">
     <div class="max-w-md w-full lg:flex self-center m-auto">
       <div class="border border-grey-light lg:border lg:border-grey-light bg-white  lg:rounded p-4 flex flex-col justify-between leading-normal">
@@ -10,27 +10,28 @@
         </div>
       </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ['modalVisible'],
   data() {
     return {
-
     }
   },
   methods: {
     close() {
-      this.$emit('closed')
+      this.$emit('modal-closed')
     }
   },
   mounted() {
     document.addEventListener("keydown", (e) => {
-      if (e.keyCode === 27) {
+      if (this.modalVisible && e.keyCode === 27) {
         this.close();
       }
     });
   }
 }
+
 </script>
