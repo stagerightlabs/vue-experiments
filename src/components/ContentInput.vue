5bed0d351ee6b864d68144b46c897cf0f2276d27
Ryan Durham <ryan@stagerightlabs.com>
2018-05-06 13:47:40 -0700

    Attempting to use contenteditable

---
+++ src/components/ContentInput.vue
@@ -0,0 +1,94 @@
+<template>
+  <span
+    @focus.capture="focus"
+    @click.capture="focus"
+    @blur.capture="blur"
+    @keydown.enter="blur"
+    @keydown.esc="escape"
+    @keydown.shift.enter.prevent="shiftEnter"
+    :tabindex="tabindex"
+    :type="type"
+    :placeholder="placeholder"
+    :aria-labelledby="ariaLabelledby"
+    :aria-describedby="ariaDescribedby"
+    :required="required"
+    :autofocus="autofocus"
+    @input="input"
+    ref='textInput'
+    style="background-color: transparent; border: none;"
+    contenteditable=""
+    >
+    {{ content }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      default: ""
+    },
+    tabindex: {
+      default: 0
+    },
+    type: {
+      default: "text"
+    },
+    placeholder: {
+      default: ""
+    },
+    ariaLabelledby: {
+      default: ""
+    },
+    ariaDescribedby: {
+      default: ""
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      editing: false,
+      content: this.value,
+      backup: ''
+    };
+  },
+  methods: {
+    sync() {
+      this.$emit("input", this.content);
+    },
+    focus() {
+      this.editing = true;
+      this.backup = this.content;
+      this.$nextTick(() => this.$refs.textInput.focus());
+    },
+    blur() {
+      this.editing = false;
+      this.sync()
+    },
+    input() {
+      this.content = this.$refs.textInput.value
+      this.sync()
+    },
+    escape() {
+      this.content = this.backup
+      this.blur();
+    },
+    shiftEnter() {
+      this.$refs.textInput.value =  this.$refs.textInput.value + "\n"
+      console.log(this.$refs.textInput.value)
+      this.input()
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Widget.vue
+++ src/views/Widget.vue
@@ -7,19 +7,19 @@
       <div class="flex my-2">
         <div class="w-1/2 text-right">Name:</div>
         <div class="w-1/2">
-          <content-text class="m-2" :value="widget.name"></content-text>
+          <input-text class="m-2" :value="widget.name"></input-text>
         </div>
       </div>
       <div class="flex my-2">
         <div class="w-1/2 text-right">Project:</div>
         <div class="w-1/2">
-          <content-text class="m-2 border-b-2 border-dotted" v-model="widget.project" placeholder="project..." tabindex="104" aria-labelledby="red1" aria-describedby="red1Description" required></content-text>
+          <input-text class="m-2 border-b-2 border-dotted" v-model="widget.project" placeholder="project..." tabindex="104" aria-labelledby="red1" aria-describedby="red1Description" required></input-text>
         </div>
       </div>
       <div class="flex my-2">
         <div class="w-1/2 text-right">Supervisor:</div>
         <div class="w-1/2">
-
+          <content-text class="m-2" v-model="widget.supervisor"></content-text>
         </div>
       </div>
     </div>
@@ -28,10 +28,12 @@
 
 <script>
 import TextInput from "@/components/TextInput.vue";
+import ContentInput from "@/components/ContentInput.vue";
 
 export default {
   components: {
-    "content-text": TextInput,
+    "input-text": TextInput,
+    "content-text": ContentInput
   },
   data() {
     return {
