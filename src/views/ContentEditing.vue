81e3a68f983b0b7aeb2dc8043dc7c6a5ac947059
Ryan Durham <ryan@stagerightlabs.com>
2018-05-12 15:35:54 -0700

    Revised example data on content-editing page

--- src/views/ContentEditing.vue
+++ src/views/ContentEditing.vue
@@ -1,65 +1,55 @@
 <template>
   <div>
     <h1>Content Editing</h1>
-    <div class="border p-4 mt-4">
+    <div class="border p-4 mt-4 text-lg">
       <div class="flex my-2">
-        <div class="w-1/2 text-right">Name:</div>
+        <div class="w-1/3 text-right font-bold">Name:</div>
         <div class="w-1/2">
-          <input-textarea class="m-2" :value="widget.name"></input-textarea>
+          <editable
+            input-class="ml-2 border-b-2 border-dotted"
+            v-model="character.name"
+          ><icon name="pencil-alt" class="text-grey w-3 h-3"></icon></editable>
         </div>
       </div>
       <div class="flex my-2">
-        <div class="w-1/2 text-right">Project:</div>
+        <div class="w-1/3 text-right font-bold">Position:</div>
         <div class="w-1/2">
-          <input-text class="m-2 border-b-2 border-dotted"
-            v-model="widget.project"
-            placeholder="project..."
-            tabindex="104"
-            aria-labelledby="red1"
-            aria-describedby="red1Description"
-            required>
-          </input-text>
+          <editable
+            input-class="ml-2 border-b-2 border-dotted"
+            v-model="character.position"
+          ><icon name="pencil-alt" class="text-grey w-3 h-3"></icon></editable>
         </div>
       </div>
       <div class="flex my-2">
-        <div class="w-1/2 text-right">Supervisor:</div>
+        <div class="w-1/3 text-right font-bold">Hobby:</div>
         <div class="w-1/2">
-          <content-text
-            class="ml-2 border-b-2 border-dotted"
-            v-model="widget.supervisor"
-          ><icon name="pencil-alt" class="text-grey w-3 h-3"></icon></content-text>
-        </div>
-      </div>
-      <div class="flex my-2">
-        <div class="w-1/2 text-right">Supervisor:</div>
-        <div class="w-1/2">
-          <input type="text" v-model="widget.supervisor">
+          <editable
+            input-class="ml-2 border-b-2 border-dotted"
+            v-model="character.hobby"
+          ><icon name="pencil-alt" class="text-grey w-3 h-3"></icon></editable>
         </div>
       </div>
     </div>
     <hr>
     <h4 class="mt-4">Object:</h4>
-    <p class="bg-grey-lighter border p-4 mx-0 font-mono">{{ widget }}</p>
+    <p class="bg-grey-lighter border p-4 mx-0 font-mono">{{ character }}</p>
   </div>
 </template>
 
 <script>
-import TextInput from '@/components/TextInput.vue';
-import TextArea from '@/components/TextArea.vue';
-import ContentInput from '@/components/ContentInput.vue';
+import Editable from '@/components/Editable.vue';
 
 export default {
+  name: 'ContentEditing',
   components: {
-    'input-text': TextInput,
-    'content-text': ContentInput,
-    'input-textarea': TextArea,
+    Editable,
   },
   data() {
     return {
-      widget: {
-        name: 'fred',
-        project: 'red herring',
-        supervisor: 'johnson',
+      character: {
+        name: 'Andrei Sergeyevich Prozorov',
+        position: 'City Council Secretary',
+        hobby: 'violin',
       },
     };
   },
