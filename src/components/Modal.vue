790941e608bd35e897a9723bb6daf9bfadd1e4d1
Ryan Durham <ryan@stagerightlabs.com>
2018-05-11 21:03:11 -0700

    Formatting fix

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="absolute fixed pin w-full h-full bg-smoke-light flex" @click.self="close">
     <div class="max-w-md w-full lg:flex self-center m-auto">
-      <div class="border border-grey-light lg:border lg:border-grey-light bg-white lg:rounded p-4 flex flex-col justify-between leading-normal w-full">
+      <div class="border border-grey-light lg:border lg:border-grey-light
+        bg-white lg:rounded p-4 flex flex-col justify-between
+        leading-normal w-full">
         <div class="mb-8">
           <span @click="close">
             <icon name="times" class="float-right cursor-pointer"></icon>
@@ -18,20 +20,20 @@ export default {
   props: ['modalVisible'],
   data() {
     return {
-    }
+    };
   },
   methods: {
     close() {
-      this.$emit('modal-closed')
-    }
+      this.$emit('modal-closed');
+    },
   },
   mounted() {
-    document.addEventListener("keydown", (e) => {
+    document.addEventListener('keydown', (e) => {
       if (this.modalVisible && e.keyCode === 27) {
         this.close();
       }
     });
-  }
-}
+  },
+};
 
 </script>
