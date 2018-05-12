e9b7d019ae9c1249ebe15319d2f67f3b1f3d81b4
Ryan Durham <ryan@stagerightlabs.com>
2018-05-11 21:01:35 -0700

    formatting fixes, add pencil icon

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
+import 'vue-awesome/icons/times';
+import 'vue-awesome/icons/pencil-alt';
+import Icon from 'vue-awesome/components/Icon.vue';
+import PortalVue from 'portal-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import 'vue-awesome/icons/times'
-import Icon from 'vue-awesome/components/Icon'
-import PortalVue from 'portal-vue'
 
 // Register the portal component
-Vue.use(PortalVue)
+Vue.use(PortalVue);
 
 // Register the font-awesome icon component
-Vue.component('icon', Icon)
+Vue.component('icon', Icon);
 
 Vue.config.productionTip = false;
 
