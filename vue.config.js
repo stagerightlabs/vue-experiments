66d5804eb5d5ba0d369ae4e14c726a180e73e80a
Ryan Durham <ryan@stagerightlabs.com>
2018-05-13 15:42:18 -0700

    Re-enable purge css with tailwind extractor

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,32 @@ const PurgecssPlugin = require('purgecss-webpack-plugin');
 const glob = require('glob-all');
 const path = require('path');
 
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
     plugins: [
-      // new PurgecssPlugin({
-      //   paths: glob.sync([
-      //     path.join(__dirname, './src/index.html'),
-      //     path.join(__dirname, './**/*.vue'),
-      //     path.join(__dirname, './src/**/*.js')
-      //   ])
-      // }),
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js'),
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ['html', 'js', 'php', 'vue'],
+          },
+        ],
+      }),
     ],
   },
 };
