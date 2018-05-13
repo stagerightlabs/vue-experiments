1b00beb09d760b1b8f0539fe2d98515be9e1ddd2
Ryan Durham <ryan@stagerightlabs.com>
2018-05-13 15:35:47 -0700

    Add a 404 component

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import NotFound from './views/NotFound.vue';
 import Modals from './views/Modals.vue';
 import Toast from './views/Toast.vue';
 import ContentEditing from './views/ContentEditing.vue';
@@ -28,6 +29,11 @@ export default new Router({
       path: '/toast-notifications',
       name: 'toast-notifications',
       component: Toast,
+    },
+    {
+      path: '*',
+      name: '404',
+      component: NotFound,
     },
   ],
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <div class="flex flex-col items-center">
+      <h1>404</h1>
+      <p>The page you are looking for does not exist!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
