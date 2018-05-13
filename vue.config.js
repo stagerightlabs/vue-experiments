59ea54540d9a0b0470622730b7d56aac5b54a91f
Ryan Durham <ryan@stagerightlabs.com>
2018-05-13 15:22:36 -0700

    Disable purgecss

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-const glob = require('glob-all')
-const path = require('path')
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const path = require('path');
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
     plugins: [
-      new PurgecssPlugin({
-        paths: glob.sync([
-          path.join(__dirname, './src/index.html'),
-          path.join(__dirname, './**/*.vue'),
-          path.join(__dirname, './src/**/*.js')
-        ])
-      }),
-    ]
-  }
+      // new PurgecssPlugin({
+      //   paths: glob.sync([
+      //     path.join(__dirname, './src/index.html'),
+      //     path.join(__dirname, './**/*.vue'),
+      //     path.join(__dirname, './src/**/*.js')
+      //   ])
+      // }),
+    ],
+  },
 };
