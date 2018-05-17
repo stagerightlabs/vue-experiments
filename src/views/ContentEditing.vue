9b20e7da5f1feed606db0398c5c89be694541511
Ryan Durham <ryan@stagerightlabs.com>
2018-05-17 13:46:26 -0700

    Add alternative examples of the editable component's usage

--- src/views/ContentEditing.vue
+++ src/views/ContentEditing.vue
@@ -6,7 +6,7 @@
         <div class="w-1/3 text-right font-bold">Name:</div>
         <div class="w-1/2">
           <editable
-            input-class="ml-2 border-b-2 border-dotted"
+            input-class="ml-2 border-b-2 border-dotted inline-block"
             v-model="character.name"
           ><icon name="pencil-alt" class="text-grey w-3 h-3"></icon></editable>
         </div>
@@ -15,21 +15,35 @@
         <div class="w-1/3 text-right font-bold">Position:</div>
         <div class="w-1/2">
           <editable
-            input-class="ml-2 border-b-2 border-dotted"
+            input-class="ml-2 border-b-2 border-dotted inline-block"
             v-model="character.position"
-          ><icon name="pencil-alt" class="text-grey w-3 h-3"></icon></editable>
+          ></editable>
         </div>
       </div>
       <div class="flex my-2">
         <div class="w-1/3 text-right font-bold">Hobby:</div>
         <div class="w-1/2">
           <editable
-            input-class="ml-2 border-b-2 border-dotted"
+            input-class="ml-2 p-1 border border-solid border-green-light bg-grey-light inline-block"
             v-model="character.hobby"
-          ><icon name="pencil-alt" class="text-grey w-3 h-3"></icon></editable>
+          ></editable>
         </div>
       </div>
     </div>
+    <p>
+      Editable content can also be displayed inline:
+      <editable
+            input-class="ml-2 border-b-2 border-dotted inline-block"
+            v-model="character.name"
+          >
+      </editable>
+      or as a block element:
+      <editable
+            input-class="border-2 border-dotted block w-64 mx-auto my-4 p-2"
+            v-model="character.position"
+          >
+      </editable>
+    </p>
     <hr>
     <h4 class="mt-4">Object:</h4>
     <p class="bg-grey-lighter border p-4 mx-0 font-mono">{{ character }}</p>
