5322a02d3241c6352e920164e061b77945cacf15
Ryan Durham <ryan@stagerightlabs.com>
2018-05-17 15:16:08 -0700

    Add a description of the editable component to the 'content editing' page

--- src/views/ContentEditing.vue
+++ src/views/ContentEditing.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <h1>Content Editing</h1>
+    <p>This component aims to add some UX/UI sugar to the "content editable" experience by providing some additional functionality:</p>
+    <ul class="mx-8 pb-8">
+      <li class="my-2">Allow data binding via <code>v-model</code></li>
+      <li class="my-2">Use the <span class="keyboard-symbol">ESC</span> key to abort content changes and restore the prior value before blurring the input.</li>
+      <li class="my-2">The <span class="keyboard-symbol">Enter</span> key will add a carriage return.</li>
+      <li class="my-2">Typing <span class="keyboard-symbol">Shift</span> + <span class="keyboard-symbol">Enter</span> will save the edits and blur the input.</li>
+      <li class="my-2">Automatic tab-focussing</li>
+      <li class="my-2">Aria descriptors, placeholders and autofocus can be specified</li>
+      <li class="my-2">Non-editable content can be displayed adjacent to the editable div via a slot; this allows us to add an indicator for better UX.</li>
+      <li class="my-2">Clicking on the slot content will focus the input.</li>
+    </ul>
+    <h4>Demo</h4>
     <div class="border p-4 mt-4 text-lg">
       <div class="flex my-2">
         <div class="w-1/3 text-right font-bold">Name:</div>
@@ -29,8 +41,7 @@
           ></editable>
         </div>
       </div>
-    </div>
-    <p>
+      <p>
       Editable content can also be displayed inline:
       <editable
             input-class="ml-2 border-b-2 border-dotted inline-block"
@@ -39,14 +50,25 @@
       </editable>
       or as a block element:
       <editable
-            input-class="border-2 border-dotted block w-64 mx-auto my-4 p-2"
+            input-class="border-2 border-dotted block w-64 mx-auto mt-4 p-2"
             v-model="character.position"
           >
       </editable>
     </p>
+    </div>
+
     <hr>
     <h4 class="mt-4">Object:</h4>
     <p class="bg-grey-lighter border p-4 mx-0 font-mono">{{ character }}</p>
+
+    <h4 class="mt-4">Usage</h4>
+    <p class="bg-grey-lighter border p-4 mx-0 font-mono">
+      <pre>&lt;editable input-class=&quot;ml-2 border-b-2 border-dotted inline-block&quot; v-model=&quot;character.name&quot;&gt;
+  &lt;icon name=&quot;pencil-alt&quot; class=&quot;text-grey w-3 h-3&quot;&gt;&lt;/icon&gt;
+&lt;/editable&gt;</pre>
+    </p>
+
+    </pre>
   </div>
 </template>
 
