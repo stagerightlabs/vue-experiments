964eda3e3681e1e53fd5518967288608d94ec2ed
Ryan Durham <ryan@stagerightlabs.com>
2018-05-18 11:51:22 -0700

    Add Typeahead search demos

--- src/views/TypeaheadSearch.vue
+++ src/views/TypeaheadSearch.vue
@@ -4,18 +4,76 @@
 
     <div class="flex mt-8">
       <div class="w-1/2">
-          <p>Add:</p>
-          <type-ahead :options="options" @select="onSelection" :allow-new="false">
+        <p>Find a character:</p>
+        <type-ahead
+          :options="options"
+          display="name"
+          @select="onSelectionA"
+          class="w-64">
           <template slot="item" slot-scope="option">
             {{ option.searchable }}
           </template>
         </type-ahead>
-        <hr>
-        <p>Chosen:</p>
-        <ul class="">
-          <li v-for="selected in selection" :key="selected.name">{{ selected.name }}</li>
-        </ul>
       </div>
+      <div class="w-1/2">
+        <p>
+          Chosen
+          <span
+            @click="reset('selectionA')"
+            class="text-grey-light cursor-pointer float-right">
+            Clear
+          </span>
+        </p>
+        <div class="border rounded min-height-32">
+          <ul v-if="selectionA.length" class="py-4 px-8">
+            <li
+              class="p-1"
+              v-for="(selected, index) in selectionA" :key="index">
+              {{ selected.name }}
+            </li>
+          </ul>
+          <p v-else class="text-grey">Nothing selected..</p>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="flex mt-8 border-t">
+      <div class="w-1/2">
+          <p>Find or create a character:</p>
+          <type-ahead
+            @select="onSelectionB"
+            class="w-64"
+            display="name"
+            :options="options"
+            :allow-new="true">
+          <template slot="item" slot-scope="option">
+            {{ option.searchable }}
+          </template>
+        </type-ahead>
+      </div>
+      <div class="w-1/2">
+        <p>
+          Chosen
+          <span
+            @click="reset('selectionB')"
+            class="text-grey-light cursor-pointer float-right">
+            Clear
+          </span>
+        </p>
+        <div class="border rounded min-height-32">
+          <ul v-if="selectionB.length" class="py-4 px-8">
+            <li
+              class="p-1"
+              :class="{'bg-green-lightest': isNewEntry(selected)}"
+              v-for="(selected, index) in selectionB"
+              :key="index">
+              {{ selected.name }}
+            </li>
+          </ul>
+          <p v-else class="text-grey">Nothing selected..</p>
+        </div>
+      </div>
+      <hr>
     </div>
   </div>
 </template>
@@ -31,27 +89,41 @@ export default {
   data() {
     return {
       options: [
-        { name: 'Olga Sergeyevna Prozorova', group: 'Prozorovs' },
-        { name: 'Maria Sergeyevna Kulygina', group: 'Prozorovs' },
-        { name: 'Irina Sergeyevna Prozorova', group: 'Prozorovs' },
-        { name: 'Andrei Sergeyevich Prozorov', group: 'Prozorovs' },
-        { name: 'Natalia Ivanovna', group: 'Prozorovs' },
-        { name: 'Fyodor Ilyich Kulygin', group: 'Prozorovs' },
-        { name: 'Aleksandr Ignatyevich Vershinin', group: 'Soldiers' },
-        { name: 'Baron Nikolaj Lvovich Tuzenbach', group: 'Soldiers' },
-        { name: 'Staff Captain Vassily Vasilyevich Solyony', group: 'Soldiers' },
-        { name: 'Ivan Romanovich Chebutykin', group: 'Soldiers' },
-        { name: 'Aleksej Petrovich Fedotik', group: 'Soldiers' },
-        { name: 'Vladimir Karlovich Rode', group: 'Soldiers' },
-        { name: 'Anfisa', group: 'Cameos' },
-        { name: 'Ferapont', group: 'Cameos' },
+        { id: 1, name: 'Olga Sergeyevna Prozorova', group: 'Prozorovs' },
+        { id: 2, name: 'Maria Sergeyevna Kulygina', group: 'Prozorovs' },
+        { id: 3, name: 'Irina Sergeyevna Prozorova', group: 'Prozorovs' },
+        { id: 4, name: 'Andrei Sergeyevich Prozorov', group: 'Prozorovs' },
+        { id: 5, name: 'Natalia Ivanovna', group: 'Prozorovs' },
+        { id: 6, name: 'Fyodor Ilyich Kulygin', group: 'Prozorovs' },
+        { id: 7, name: 'Aleksandr Ignatyevich Vershinin', group: 'Soldiers' },
+        { id: 8, name: 'Baron Nikolaj Lvovich Tuzenbach', group: 'Soldiers' },
+        { id: 9, name: 'Staff Captain Vassily Vasilyevich Solyony', group: 'Soldiers' },
+        { id: 10, name: 'Ivan Romanovich Chebutykin', group: 'Soldiers' },
+        { id: 11, name: 'Aleksej Petrovich Fedotik', group: 'Soldiers' },
+        { id: 12, name: 'Vladimir Karlovich Rode', group: 'Soldiers' },
+        { id: 13, name: 'Anfisa', group: 'Cameos' },
+        { id: 14, name: 'Ferapont', group: 'Cameos' },
       ],
-      selection: [],
+      selectionA: [],
+      selectionB: [],
     };
   },
   methods: {
-    onSelection(selected) {
-      this.selection.push(selected);
+    onSelectionA(selected) {
+      this.selectionA.push(selected);
+    },
+    onSelectionB(selected) {
+      this.selectionB.push(selected);
+    },
+    reset(name) {
+      if (!Object.prototype.hasOwnProperty.call(this, name)) {
+        return;
+      }
+
+      this[name] = [];
+    },
+    isNewEntry(selection) {
+      return !('id' in selection);
     },
   },
 };
