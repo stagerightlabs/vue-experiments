f0ab834623026100d4d6cf226361e590ea6a9ca8
Ryan Durham <ryan@stagerightlabs.com>
2018-05-18 13:33:51 -0700

    Add descriptive text to the TypeaheadSearch page

--- src/views/TypeaheadSearch.vue
+++ src/views/TypeaheadSearch.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
     <h1>Typeahead Search</h1>
-
+    <p>
+      The goal here is to allow users to make selections from a pre-defined list
+      of options.  Those options could be fetched via ajax or stored inline, as
+      we do here. When a selection is made an event is fired notifying the parent
+      component of the selected option.  It is the responsibility of the parent
+      to check for duplicates and/or reduce the option set based on that selection.
+    </p>
+    <ul>
+      <li class="p-1">
+        You can decide whether or not to allow users to create new options via
+        the <code>allow-new</code> prop.
+      </li>
+      <li class="p-1">
+        The <code>display</code> prop allows you to specify which property on
+        the option object should be used in the dropdown display.
+      </li>
+      <li class="p-1">
+        Change the look and feel of the input via the <code>input-class</code> prop.
+      </li>
+      <li class="p-1">
+        An <code>invalid-selection</code> event is fired if the user selects an
+        invalid option and the creation of new records is turned off.
+      </li>
+    </ul>
     <div class="flex mt-8">
       <div class="w-1/2">
         <p>Find a character:</p>
@@ -9,10 +32,10 @@
           :options="options"
           display="name"
           @select="onSelectionA"
-          class="w-64">
-          <template slot="item" slot-scope="option">
-            {{ option.searchable }}
-          </template>
+          @invalid-selection="invalidSelection"
+          class="w-64"
+          input-class="border border-green text-green-dark py-3 px-4 pr-8 bg-green-lightest"
+          >
         </type-ahead>
       </div>
       <div class="w-1/2">
@@ -37,18 +60,15 @@
       </div>
     </div>
     <hr>
-    <div class="flex mt-8 border-t">
+    <div class="flex my-8 border-t">
       <div class="w-1/2">
-          <p>Find or create a character:</p>
-          <type-ahead
-            @select="onSelectionB"
-            class="w-64"
-            display="name"
-            :options="options"
-            :allow-new="true">
-          <template slot="item" slot-scope="option">
-            {{ option.searchable }}
-          </template>
+        <p>Find or create a character:</p>
+        <type-ahead
+          @select="onSelectionB"
+          class="w-64"
+          display="name"
+          :options="options"
+          :allow-new="true">
         </type-ahead>
       </div>
       <div class="w-1/2">
@@ -64,17 +84,17 @@
           <ul v-if="selectionB.length" class="py-4 px-8">
             <li
               class="p-1"
-              :class="{'bg-green-lightest': isNewEntry(selected)}"
               v-for="(selected, index) in selectionB"
               :key="index">
               {{ selected.name }}
+              <span v-if="isNewEntry(selected)" class="text-green-darker">[added by user]</span>
             </li>
           </ul>
           <p v-else class="text-grey">Nothing selected..</p>
         </div>
       </div>
-      <hr>
     </div>
+    <p class="mt-8">It would be nice if we could allow customized styling of the <code>UL</code> and <code>LI</code> tags used in the options list.</p>
   </div>
 </template>
 
@@ -124,6 +144,10 @@ export default {
     },
     isNewEntry(selection) {
       return !('id' in selection);
+    },
+    invalidSelection(invalid) {
+      // eslint-disable-next-line no-alert
+      alert(`'${invalid}' is not a valid option.`);
     },
   },
 };
