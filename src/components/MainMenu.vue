69c4eb77e4ec7843deeab0b77e9bcb3bc4ea567b
Ryan Durham <ryan@stagerightlabs.com>
2018-07-04 20:33:23 -0700

    Restore gitlab badge

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -33,11 +33,11 @@
         href="https://gitlab.com/stage-right-labs/vue-experiments"
         target="_blank"
       > <icon name="brands/gitlab" class="w-3 h-3"></icon> View Source</a>
-      <!-- <img
+      <img
         src="https://gitlab.com/stage-right-labs/vue-experiments/badges/master/build.svg"
         aria-hidden="true"
         class="project-badge"
-      > -->
+      >
     </p>
   </div>
 </template>
